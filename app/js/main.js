d96b4ae0d67e71c420299235e7b3d2b37bb6f29d
daria07 <d.ilina07@yandex.ru>
2019-03-05 19:34:33 +0300

    тестовое

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,20 @@
 jQuery(document).ready(function($){
 	var swiper = new Swiper('.swiper-container', {
 		slidesPerView: 3,
-			spaceBetween: 30,
-			breakpoints: {
-				
-				409: {
-					slidesPerView: 1,
-					spaceBetween: 0,
-					width: 250
-				},
-				991: {
-					slidesPerView: 2,				
-					spaceBetween: 15,
-					width: 307,
-				}
-			}	
+		spaceBetween: 30,
+		breakpoints: {
+			991: {
+				slidesPerView: 1,				
+				spaceBetween: 15,
+				width: 307
+			},
+			767: {
+				slidesPerView: 1,
+				spaceBetween: 10,
+				width: 220
+			},
+			
+		}	
 	});
 	
 	$('.fancybox').fancybox();
